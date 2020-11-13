3d11e0eb4a79e01049fa6c6e3ae79c6348b7e489
Radu Buzatu <48164997+buzaturadu@users.noreply.github.com>
2020-11-14 00:22:29 +0200

    Create emailer.js

---
+++ emailer.js
@@ -0,0 +1,58 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const fs = require("fs");
+
+
+listOfPaths = ['E:\Codes\VS\testfiles\test.json'];
+
+function transformToAttachment() {
+
+    arrayOfAttachments = [];
+
+    for (path of listOfPaths) {
+        var split = path.split('/');
+
+        var fileName = split[split.length-1];
+        var extention = fileName.substring(fileName.indexOf('.') + 1, fileName.length);
+
+        arrayOfAttachments.push({
+            fileName: fileName,
+            extention: extention
+        });
+    }
+
+    return arrayOfAttachments;
+}
+
+
+const msg = {
+  to: [ 'buzatu.radu@gmail.com' ] ,
+  from: 'radu@sjultra.com', 
+  subject: 'Mars reports',
+  text: 'Reports in attachment',
+  html: '<strong>Reports in attachment</strong>',
+  attachments: transformToAttachment,
+};
+
+sgMail
+  .send(msg)
+  .then(() => {}, error => {
+    console.error(error);
+ 
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  });
+//ES8
+(async () => {
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    console.error(error);
+ 
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  }
+})();
